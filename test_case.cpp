f0bdfa4909f6739cd432d37004976eb2c9b74d07
APAC\CJA2KOR <cja2kor@bosch.com>
2022-09-04 18:31:02 +0530

    Add checker code for Parameters of BatteryStatus

--- test_case.cpp
+++ test_case.cpp
@@ -9,8 +9,20 @@ using namespace BatteryStatus;
 
 
 TEST_CASE("TEST_CASE 1 :: Generate Parameters of Battery Temperature") {
-  Battery batteryobj;
   
+    Battery batteryobj;
+    std::srand(static_cast<unsigned int>(std::time(nullptr))); 
+  
+    for (int count=1; count <= 25; ++count)
+    {
+        float randval =  std::rand()%100;
+        assert(batteryobj.batteryIsOk(randval,TEMPERATURE) == true);
+        assert(batteryobj.batteryIsOk(randval,SOC) == true);
+	}
+}
+TEST_CASE("TEST_CASE 2 :: Generate Parameters of Battery SOC") {
+  
+    Battery batteryobj;
     std::srand(static_cast<unsigned int>(std::time(nullptr))); 
   
     for (int count=1; count <= 25; ++count)
