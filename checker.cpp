fa5b4114a76b250fede0e21005d64cac21b44ef4
APAC\CJA2KOR <cja2kor@bosch.com>
2022-09-04 17:21:57 +0530

    Add checker code for Parameters of BatteryStatus

--- checker.cpp
+++ checker.cpp
@@ -3,17 +3,15 @@
 #include "checker.hpp"
 using namespace std;
 using namespace BatteryStatus;
-void printError(std::string str){
-	cout<<str << " out of range! "<<endl;
-}
+
 void printoutputtocommunicate(std::vector<float>ParametersofBattery){
 	 for (int Parameters : ParametersofBattery) {
         std::cout << Parameters << ", ";
     }
+	std::cout <<'\n';
 }
-bool ChecksStatus(int min,int max,float value, std::string str){
+bool ChecksStatus(int min,int max,float value){
 	if(value < min || value > max) {
-    printError(str);
     return false;
 	}
 	return true;
@@ -30,10 +28,10 @@ bool Battery::batteryIsOk(std::vector<float>ParametersofBatteryStatus) {
 	return true;
 }
 bool Battery::batteryTemperatureIsOk(float temperature){
-	return ChecksStatus(MINTEMPERATURE,MAXTEMPERATURE,temperature,"Temperature");
+	return ChecksStatus(MINTEMPERATURE,MAXTEMPERATURE,temperature);
 }
 bool Battery::batterysocIsOk(float soc){
-	 return ChecksStatus(MINSOC,MAXSOC,soc,"State of Charge");
+	 return ChecksStatus(MINSOC,MAXSOC,soc);
 }
 
 
