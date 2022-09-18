aabdebc3cce23e854813e234d75d4f243c0460fe
baburoshima <105203651+baburoshima@users.noreply.github.com>
2022-09-18 21:56:47 +0530

    Receiver implementation

--- Receiver.py
+++ Receiver.py
@@ -1,13 +1,53 @@
 import sys
+from itertools import chain
 
-BatteryParameters = ["----------Temperature Sensor data----------", "----------Soc Sensor data----------"]
+Parameter1_ConsolePrint = ['----------Temperature Sensor data----------']
+Parameter2_ConsolePrint =['----------Soc Sensor data----------']
 
-def ReadBatteryParameterDataFromConsole():
+def ReadBatteryParameterReadingFromConsole():
     LinesRead = sys.stdin.readlines()
     StrippedData = []
     for Reading in LinesRead:
       Reading = Reading.strip('\n')
-      ReadingList = list(map(float,Reading.split(',')))
+      ReadingList = list(Reading.split(','))
       StrippedData.append(ReadingList)
-    print(StrippedData)
-    return StrippedData
+    return getSensorReadings(StrippedData)
+
+def getSensorReadings(Reading):
+    # use output string to split 2 parameter readings
+    Split_Parameter1Readings = Reading[Reading.index(Parameter1_ConsolePrint)+1:Reading.index(Parameter2_ConsolePrint)] 
+    Split_Parameter2Readings = Reading[Reading.index(Parameter2_ConsolePrint)+1:]
+    Parameter1Readings = ConverttoNumArray(Split_Parameter1Readings) 
+    Parameter2Readings = ConverttoNumArray(Split_Parameter2Readings) 
+    return Parameter1Readings , Parameter2Readings
+
+def ConverttoNumArray(ParameterReadings):
+    Parameter_Combinedlist = list(chain(*ParameterReadings))
+    Parameter_NumArray = [int(i) for i in Parameter_Combinedlist]
+    return Parameter_NumArray
+
+def getMinMax_Value(ParameterReading):
+    MinMaxReading =  min(ParameterReading), max(ParameterReading) 
+    return MinMaxReading
+
+def getMovingAverage(ParamterReading, window_size) :
+    window = ParamterReading[-window_size:]
+    movingAverage = round((sum(window)/window_size),2)
+    return movingAverage
+
+def ComputeStatistics(ParameterList):
+    MinMaxReading = getMinMax_Value(ParameterList)
+    movingAverage = getMovingAverage(ParameterList,window_size = 5)
+    return MinMaxReading , movingAverage
+
+def ReceiverOutput(ParameterList) :
+    for message in range(0,len(ParameterList[1])):
+      print(ParameterList[1][message])
+      print(' Mininum Reading :', ComputeStatistics(ParameterList[0][message])[0][0], 'Maximum Reading :', ComputeStatistics(ParameterList[0][message])[0][1])
+      print(' Moving Average :',ComputeStatistics(ParameterList[0][message])[1])
+    
+
+if __name__ == "__main__":  #pragma no cover
+   Paramter1Reading , Parameter2Reading = ReadBatteryParameterReadingFromConsole()
+   ParameterList = [Paramter1Reading , Parameter2Reading ] ,[Parameter1_ConsolePrint,Parameter2_ConsolePrint]
+   ReceiverOutput(ParameterList)
---
+++ testReceiver.py
@@ -0,0 +1,34 @@
+import  unittest
+from Receiver import *
+
+def setup_senderdata():
+   testFileName = 'SenderData.txt'
+   with open(testFileName,'r') as viewFileOpen:
+       consoleoutput = viewFileOpen.read()
+       print(consoleoutput)
+
+class BMS_Receiver(unittest.TestCase):
+  def test_getSensorReadings_from_StrippedConsoleOutput(self):
+    strippedData = [['----------Temperature Sensor data----------'], ['19'], ['29'], ['7'], ['34'], ['40'], ['35'], ['22'], ['25'], ['20'], ['22'], ['----------Soc Sensor data----------'], ['34'], ['59'], ['49'], ['37'], ['27'], ['34'], ['62'], ['44'], ['77'], ['21']]
+    self.assertTrue(getSensorReadings(strippedData)[0]==[19, 29, 7, 34, 40, 35, 22, 25, 20 , 22])
+    self.assertTrue(getSensorReadings(strippedData)[1]==[34, 59, 49, 37, 27, 34, 62, 44, 77, 21])
+   
+  def test_ConvertString_to_NumArray(self):
+    self.assertTrue(ConverttoNumArray([['19'], ['29'], ['7'], ['34'], ['40'], ['35'], ['22'], ['25'], ['20'], ['22']])==[19, 29, 7, 34, 40, 35, 22, 25, 20, 22])
+    self.assertTrue(ConverttoNumArray([['34'], ['59'], ['49'], ['37'], ['27'], ['34'], ['62'], ['44'], ['77'], ['21']])==[34, 59, 49, 37, 27, 34, 62, 44, 77, 21])
+  
+  def test_getMinMaxReading(self):
+    self.assertTrue(getMinMax_Value([19, 29, 7, 34, 40, 35, 22, 25, 20, 22])==(7,40))
+    self.assertTrue(getMinMax_Value([34, 59, 49, 37, 27, 34, 62, 44, 77, 21])==(21,77))
+
+  def test_getMovingAverage(self):
+    self.assertTrue(getMovingAverage([19, 29, 7, 34, 40, 35, 22, 25, 20, 22],5)==24.8)
+    self.assertTrue(getMovingAverage([34, 59, 49, 37, 27, 34, 62, 44, 77, 21],5)==47.6)
+
+  def test_ComputeStatistics(self):
+    self.assertTrue(ComputeStatistics([19, 29, 7, 34, 40, 35, 22, 25, 20, 22])==((7,40),24.8))
+    self.assertTrue(ComputeStatistics([34, 59, 49, 37, 27, 34, 62, 44, 77, 21])==((21,77),47.6))
+
+if __name__ == "__main__":  #pragma no cover
+    setup_senderdata()
+    unittest.main()
